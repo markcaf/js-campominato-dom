1196036175727d4bec236493f460bed3c3a53372
markcaf <marcocafassog@gmail.com>
2022-06-24 18:17:40 +0200

    Fixed indentation and added bomb icon

--- js/script.js
+++ js/script.js
@@ -6,130 +6,133 @@ console.log(gridContainer);
 const playGame = document.querySelector("header button.btn");
 console.log(playGame);
 
-// Al click sul pulsante Play viene mostrata la griglia e resettata se è stata già caricata
+// Al click sul pulsante Play viene mostrata la griglia e resettata se è stata già caricata per ogni livello
 playGame.addEventListener("click", function(){
 
-// Dichiaro la variabile del select per il cambio il livello di difficoltà
-const selectLevel = document.querySelector('.form-select');
+    // Dichiaro la variabile del select per il cambio il livello di difficoltà
+    const selectLevel = document.querySelector('.form-select');
 
-// Dichiaro le variabili per la funzione di generazione di 16 numeri random
-// In base al livello e al suo range utilizzo la funzione con argomenti differenti
-let generateBombEasy = randomBombNumber(1, 100);
-let generateBombMedium = randomBombNumber(1, 81);
-let generateBombHard = randomBombNumber(1, 49)
+    // Dichiaro le variabili per la funzione di generazione di 16 numeri random
+    // In base al livello e al suo range utilizzo la funzione con argomenti differenti
+    let generateBombEasy = randomBombNumber(1, 100);
+    let generateBombMedium = randomBombNumber(1, 81);
+    let generateBombHard = randomBombNumber(1, 49)
 
-// Creo una variabile di stato per verificare la vittoria/sconfitta del giocatore
-let block = false;
+    // Creo una variabile di stato per verificare la vittoria/sconfitta del giocatore
+    let block = false;
 
-    if (selectLevel.value == 1){
-        document.getElementById("grid-container").innerHTML = "";
-        block = false;
+        if (selectLevel.value == 1){
+            document.getElementById("grid-container").innerHTML = "";
+            block = false;
 
-        console.log(generateBombEasy);
+            console.log(generateBombEasy);
 
-        gridContainer.classList.add("easy");
-        gridContainer.classList.remove("medium", "hard");
+            gridContainer.classList.add("easy");
+            gridContainer.classList.remove("medium", "hard");
 
-        // Creo un ciclo FOR per il numero di quadrati da generare
-        for (let i=0; i<100; i++){
-            const newSquare = createNewSquare();
+            // Creo un ciclo FOR per il numero di quadrati da generare
+            for (let i=0; i<100; i++){
+                const newSquare = createNewSquare();
 
-            // newSquare.innerHTML = i + 1;
+                // newSquare.innerHTML = i + 1;
 
-            newSquare.addEventListener("click", function(){
-                if (block === false){
-                    newSquare.classList.add("clicked");
-                    if (generateBombEasy.includes(i + 1)){
-                        newSquare.classList.remove("clicked");
-                        newSquare.classList.add("bomb");
-                        console.log("HAI PRESO UNA BOMBA!");
-                        block = true;
-                        if (document.querySelectorAll(".clicked").length == (100 - 16)){
-                            alert("Complimenti, hai vinto!");
-                        } else{
-                            alert("Hai preso una bomba e la partita è finita, hai totalizzato " + document.querySelectorAll(".clicked").length + " punti");
-                            }
+                newSquare.addEventListener("click", function(){
+                    if (block === false){
+                        newSquare.classList.add("clicked");
+                        if (generateBombEasy.includes(i + 1)){
+                            newSquare.classList.remove("clicked");
+                            newSquare.classList.add("bomb");
+                            newSquare.innerHTML = '<i class="fa-solid fa-bomb"></i>';
+                            console.log("HAI PRESO UNA BOMBA!");
+                            block = true;
+                            if (document.querySelectorAll(".clicked").length == (100 - 16)){
+                                alert("Complimenti, hai vinto!");
+                            } else{
+                                alert("Hai preso una bomba e la partita è finita, hai totalizzato " + document.querySelectorAll(".clicked").length + " punti");
+                                }
+                        }
+                        console.log("Hai cliccato il numero " + (i + 1));
                     }
-                    console.log("Hai cliccato il numero " + (i + 1));
-                }
-            });
+                });
 
-            gridContainer.append(newSquare)
-        }
-    }
-
-    if (selectLevel.value == 2){
-        document.getElementById("grid-container").innerHTML = "";
-        block = false;
-
-        console.log(generateBombMedium);
-
-        gridContainer.classList.add("medium");
-        gridContainer.classList.remove("easy", "hard");
-
-        // Creo un ciclo FOR per il numero di quadrati da generare
-        for (let i=0; i<81; i++){
-        const newSquare = createNewSquare();
-    
-        // newSquare.innerHTML = i + 1;
-    
-        newSquare.addEventListener("click", function(){
-            if (block === false){
-                newSquare.classList.add("clicked");
-                if (generateBombMedium.includes(i + 1)){
-                    newSquare.classList.remove("clicked");
-                    newSquare.classList.add("bomb");
-                    console.log("HAI PRESO UNA BOMBA!");
-                    block = true;
-                    if (document.querySelectorAll(".clicked").length == (81 - 16)){
-                        alert("Complimenti, hai vinto!");
-                    } else{
-                        alert("Hai preso una bomba e la partita è finita, hai totalizzato " + document.querySelectorAll(".clicked").length + " punti");
-                        }
-                }
-                console.log("Hai cliccato il numero " + (i + 1));
+                gridContainer.append(newSquare)
             }
-        });
-    
-            gridContainer.append(newSquare)
         }
-    }
 
-    if (selectLevel.value == 3){
-        document.getElementById("grid-container").innerHTML = "";
+        if (selectLevel.value == 2){
+            document.getElementById("grid-container").innerHTML = "";
+            block = false;
 
-        console.log(generateBombHard);
+            console.log(generateBombMedium);
 
-        gridContainer.classList.add("hard");
-        gridContainer.classList.remove("medium", "easy");
+            gridContainer.classList.add("medium");
+            gridContainer.classList.remove("easy", "hard");
 
-        // Creo un ciclo FOR per il numero di quadrati da generare
-        for (let i=0; i<49; i++){
-        const newSquare = createNewSquare();
-    
-        // newSquare.innerHTML = i + 1;
-    
-        newSquare.addEventListener("click", function(){
-            if (block === false){
-                newSquare.classList.add("clicked");
-                if (generateBombHard.includes(i + 1)){
-                    newSquare.classList.remove("clicked");
-                    newSquare.classList.add("bomb");
-                    console.log("HAI PRESO UNA BOMBA!");
-                    block = true;
-                    if (document.querySelectorAll(".clicked").length == (49 - 16)){
-                        alert("Complimenti, hai vinto!");
-                    } else{
-                        alert("Hai preso una bomba e la partita è finita, hai totalizzato " + document.querySelectorAll(".clicked").length + " punti");
+            // Creo un ciclo FOR per il numero di quadrati da generare
+            for (let i=0; i<81; i++){
+                const newSquare = createNewSquare();
+            
+                // newSquare.innerHTML = i + 1;
+            
+                newSquare.addEventListener("click", function(){
+                    if (block === false){
+                        newSquare.classList.add("clicked");
+                        if (generateBombMedium.includes(i + 1)){
+                            newSquare.classList.remove("clicked");
+                            newSquare.classList.add("bomb");
+                            newSquare.innerHTML = '<i class="fa-solid fa-bomb"></i>';
+                            console.log("HAI PRESO UNA BOMBA!");
+                            block = true;
+                            if (document.querySelectorAll(".clicked").length == (81 - 16)){
+                                alert("Complimenti, hai vinto!");
+                            } else{
+                                alert("Hai preso una bomba e la partita è finita, hai totalizzato " + document.querySelectorAll(".clicked").length + " punti");
+                                }
                         }
-                }
-                console.log("Hai cliccato il numero " + (i + 1));
+                        console.log("Hai cliccato il numero " + (i + 1));
+                    }
+                });
+        
+                gridContainer.append(newSquare)
             }
-        });
-    
-        gridContainer.append(newSquare)
         }
-    }
+
+        if (selectLevel.value == 3){
+            document.getElementById("grid-container").innerHTML = "";
+
+            console.log(generateBombHard);
+
+            gridContainer.classList.add("hard");
+            gridContainer.classList.remove("medium", "easy");
+
+            // Creo un ciclo FOR per il numero di quadrati da generare
+            for (let i=0; i<49; i++){
+                const newSquare = createNewSquare();
+            
+                // newSquare.innerHTML = i + 1;
+            
+                newSquare.addEventListener("click", function(){
+                    if (block === false){
+                        newSquare.classList.add("clicked");
+                        if (generateBombHard.includes(i + 1)){
+                            newSquare.classList.remove("clicked");
+                            newSquare.classList.add("bomb");
+                            newSquare.innerHTML = '<i class="fa-solid fa-bomb"></i>';
+                            console.log("HAI PRESO UNA BOMBA!");
+                            block = true;
+                            if (document.querySelectorAll(".clicked").length == (49 - 16)){
+                                alert("Complimenti, hai vinto!");
+                            } else{
+                                alert("Hai preso una bomba e la partita è finita, hai totalizzato " + document.querySelectorAll(".clicked").length + " punti");
+                                }
+                        }
+                        console.log("Hai cliccato il numero " + (i + 1));
+                    }
+                });
+        
+                gridContainer.append(newSquare)
+            }
+        }
 })
 
 
